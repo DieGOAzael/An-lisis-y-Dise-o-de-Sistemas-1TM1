2dfc88d85e037b07921dfce7d2951fc8a0179253
DieGOAzael <72054838+DieGOAzael@users.noreply.github.com>
2023-02-14 11:09:04 -0600

    Add files via upload

---
+++ Hola1.cpp
@@ -0,0 +1,14 @@
+//Librería escencial para el uso de C
+#include <iostream>
+
+//esto es para evitar el .h en el uso de la libreria
+using namespace std;
+int nombreVar;
+int main(){
+
+    //para imprimir en pantalla
+   cout<<"Hola buenos dias *_*/ \nFeliz dia del amor y la amistad \owo/";
+   return 0;
+   //obtención de datos, que viene siendo el escaner en java creo
+   cin>>nombreVar;
+}
---
+++ Programa_Uno.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"******************"<<endl;
+   cout<<"Perez Paz Diego Azael"<<endl;
+   cout<<"1TM1"<<endl;
+   cout<<"Analisis y diseo de sistemas"<<endl;
+
+   return 0;
+
+}
