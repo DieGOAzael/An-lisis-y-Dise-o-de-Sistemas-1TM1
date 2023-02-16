143f2c1ac27fbd18ee1e097f2cc55d3f3efc2243
DieGOAzael <72054838+DieGOAzael@users.noreply.github.com>
2023-02-16 11:22:15 -0600

    Add files via upload

---
+++ Sumita.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int num1, num2, suma;
+
+
+
+    cout<< "******Suma******"<<endl;
+
+    cout<< "Hagamos una suma"<<endl;
+    cout << "Introduce un primer numero: "<<endl;
+    cin >> num1;
+    cout << "Introduce un segundo numero: "<<endl;
+    cin >> num2;
+    suma = num1 + num2;
+    cout << "El resultado es: " << suma << " siuuuu.";
+
+
+    return 0;
+
+
+
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int Num;
+    float Deci;
+
+    cout<<"******Bienvenido******"<<endl;
+
+    cout<<"Introduce un numero: ";
+    cin>> Num;
+    cout<<"El numero es " << Num << "."<<endl;
+
+    cout<<"Introduce un numero decimal: ";
+    cin>> Deci;
+    cout<<"El numero es " << Deci << " .";
+
+    return 0;
+
+
+
+}
